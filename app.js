f497ca01020b3b4fd5a544f1018253bfea3d4b41
NicoExcalibur <nico.zanz@gmail.com>
2021-06-15 10:30:11 +0200

    display shadows

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
+const walk = 100; // 100px
 
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
@@ -10,6 +11,14 @@ function shadow(e) {
     x = x + e.target.offsetLeft;
     y = y + e.target.offsetTop;
   }
+
+  const xWalk = (x / width) * walk - walk / 2;
+  const yWalk = (y / width) * walk - walk / 2;
+
+  text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 rgba(255, 0, 255, 0.7),
+    ${xWalk * -1}px ${yWalk}px 0 rgba(0, 255, 0, 0.7)
+    `;
 }
 
 hero.addEventListener("mousemove", shadow);

14fb7bc52438bb27679b5c2727e3af1313cae06b
NicoExcalibur <nico.zanz@gmail.com>
2021-06-15 10:23:03 +0200

    avoid get coordonnates from the h1

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
   let { offsetX: x, offsetY: y } = e;
 
-  console.log(x, y);
+  // to avoid get near 0 coordonnates on h1
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
 }
 
 hero.addEventListener("mousemove", shadow);
